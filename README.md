9703e358aa7f7f419a6e7034438998bfb81a865f
neffej <118024182+neffej@users.noreply.github.com>
2023-01-09 09:55:44 -0500

    changed clear itinerary text display

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # happy-workaway-aday
+
+
+## Description
+
 This is a Work Day Scheduler that utilizes JQuery, Bootstrap and Days.js to allow a user to input and save items into an itinerary.
+
+
+## Current Bugs
+*The clear itinerary button works in Live Server but not in deployed webpage.
+
+## Installation
+The website is accessible at the links below. You can navigate there with any functional web browser. The website should apper as in the image below:
+
+![The deployed website matches the mock-up provided in the service ticket. ](./assets/screencapture.png)
+
+
+
+## Usage
+Navigate to the webpage. Fill in the columns with itinerary items and todos, then press the save button to make your input persist.
+
+## Links
+*https://github.com/neffej/happy-workaway-aday
+
+*https://neffej.github.io/fuzzy-pw-system/
+
+
+
+### References
--- assets/script/script.js
+++ assets/script/script.js
@@ -50,15 +50,15 @@ $('.fas').on('click',function(){
 // Click event - clear localStorage and display empty itinerary
 $('.clearBtn').on('click',function(){
   localStorage.clear();
-  $('#hour-9').children('.description').text(localStorage.getItem('hour-9'))
-$('#hour-10').children('.description').text(localStorage.getItem('hour-10'))
-$('#hour-11').children('.description').text(localStorage.getItem('hour-11'))
-$('#hour-12').children('.description').text(localStorage.getItem('hour-12'))
-$('#hour-13').children('.description').text(localStorage.getItem('hour-13'))
-$('#hour-14').children('.description').text(localStorage.getItem('hour-14'))
-$('#hour-15').children('.description').text(localStorage.getItem('hour-15'))
-$('#hour-16').children('.description').text(localStorage.getItem('hour-16'))
-$('#hour-17').children('.description').text(localStorage.getItem('hour-17'))
+  $('#hour-9').children('.description').text('')
+$('#hour-10').children('.description').text('')
+$('#hour-11').children('.description').text('')
+$('#hour-12').children('.description').text(('')
+$('#hour-13').children('.description').text('')
+$('#hour-14').children('.description').text('')
+$('#hour-15').children('.description').text('')
+$('#hour-16').children('.description').text('')
+$('#hour-17').children('.description').text('')
 })
 
 // Retrieve information and populate textarea with LocalStorage values
